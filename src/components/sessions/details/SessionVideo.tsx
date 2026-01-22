f248c5b034cd943054a7557e45ab81218e0c2246
Gaurav Agarwal <algogrit@gmail.com>
2026-01-22 09:27:47 +0530

    Show video status only for recorded sessions.

--- src/components/sessions/details/SessionVideo.tsx
+++ src/components/sessions/details/SessionVideo.tsx
@@ -1,9 +1,13 @@
-import type { Session } from "../../../data/Session";
+import { SESSION_STATUS, type Session } from "../../../data/Session";
 import { VIDEO_PLATFORM } from "../../../data/video";
 
 export default function SessionVideo({ session }: { session: Session }) {
   if(!session.data.resources?.videos) {
-    return <p className="text-2xl text-center font-bold text-codermana-orange my-8">video will be uploaded soon</p>;
+    if(session.status != SESSION_STATUS.RECORDED) {
+      return null;
+    }
+
+    return <p className="text-2xl text-center text-codermana-orange my-8">video will be uploaded soon</p>;
   }
 
   const youtube = session.data.resources?.videos?.find(
